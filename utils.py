d2bc9f532049bdee03176023bf0fda7db6da749e
Paul R Saxman <saxman@gmail.com>
2024-06-18 15:50:26 -0700

    Added pre-quantization of large models

--- utils.py
+++ utils.py
@@ -1,24 +1,24 @@
 def print_model_info(model):
-    print(f'Model size : {model.get_memory_footprint() // 1024 ** 2} MB')
+    print(f'model : size : {model.get_memory_footprint() // 1024 ** 2} MB')
 
     try:
-        print(f'Model is quantized : {model.is_quantized}')
-        print(f'Model quantization method : {model.quantization_method}')
+        print(f'model : is quantized : {model.is_quantized}')
+        print(f'model : quantization method : {model.quantization_method}')
     except:
-        print(f'Model is quantized : False')
+        print(f'model : is quantized : False')
         pass
         
     try:
-        print(f'Model 8-bit quantized : {model.is_loaded_in_8bit}')
+        print(f'model : 8-bit quantized : {model.is_loaded_in_8bit}')
     except:
         pass
     
     try:
-        print(f'Model 4-bit quantized : {model.is_loaded_in_4bit}')
+        print(f'model : 4-bit quantized : {model.is_loaded_in_4bit}')
     except:
         pass
     
-    print(f'Model on GPU : {next(model.parameters()).is_cuda}')
+    print(f'model : on GPU : {next(model.parameters()).is_cuda}')
 
 def print_device_info():
     import pynvml
